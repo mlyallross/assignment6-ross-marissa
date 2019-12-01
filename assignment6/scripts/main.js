17ad4047be51ebd1dcf4714b80c69c1ccc8e2e91
mlyallross <58403546+mlyallross@users.noreply.github.com>
2019-12-01 14:50:57 -0700

    Add files via upload

---
+++ assignment6/scripts/main.js
@@ -0,0 +1,25 @@
+//Array
+var members = [
+    'Dave Grohl',
+    'Nate Mendel',
+    'Pat Smear',
+    'Taylor Hawkins',
+    'Rami Jaffee',
+    'Chris Shiflett',
+  ];
+
+//Object
+var tour = {
+  chicago: "July 29th, 2018",
+  seattle: "September 1, 2018",
+  edmonton: "September 4, 2018",
+};
+
+//Loop
+var x = 0;
+
+while (x < 5) {
+  console.log ('Times running through the loop x: '+ x);
+
+  x++;
+}
